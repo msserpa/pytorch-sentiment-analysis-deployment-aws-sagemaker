2957351d5d400a812a5e03ae07e1a03d2361d4f0
Matheus Serpa <msserpa@inf.ufrgs.br>
2021-02-10 22:15:11 +0000

    deploying inference

--- serve/predict.py
+++ serve/predict.py
@@ -65,13 +65,8 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    # Process input_data so that it is ready to be sent to our model.
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +79,9 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    # Compute the result of applying the model to the input data.
+    with torch.no_grad():
+        output = model(data)
+    result = np.round(output.numpy())
 
     return result
